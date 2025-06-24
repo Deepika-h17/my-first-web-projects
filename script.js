ad8f095b0e05703405c63e5a4e18e0b2dbb3ff62
Deepika H <hdeepika81@jnn.edu.in>
2025-06-24 20:55:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function append(value) {
-  document.getElementById("display").value += value;
-}
-
-function clearDisplay() {
-  document.getElementById("display").value = '';
-}
-
-function calculate() {
-  try {
-    document.getElementById("display").value = eval(document.getElementById("display").value);
-  } catch (e) {
-    document.getElementById("display").value = "Error";
-  }
+function add() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+  document.getElementById("result").innerText = "Result: " + (n1 + n2);
 }
