1f856c8ed06cb0d32d09e500a4158ad0a5f8bee7
Deepika H <hdeepika81@jnn.edu.in>
2025-06-25 08:17:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function add() {
-  const n1 = parseFloat(document.getElementById("num1").value);
-  const n2 = parseFloat(document.getElementById("num2").value);
-  document.getElementById("result").innerText = "Result: " + (n1 + n2);
+function calculate() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var operator = document.getElementById("operator").value;
+  var result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = "Please enter valid numbers!";
+  } else {
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num2 !== 0 ? (num1 / num2) : "Can't divide by 0!";
+        break;
+      default:
+        result = "Invalid operation";
+    }
+  }
+
+  document.getElementById("result").innerText = "Result: " + result;
 }
